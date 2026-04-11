13f97b7ea22e298faea3767d3f91a8220a974818
singh2034 <singhaayush2034@gmail.com>
2026-04-11 22:02:41 +0530

    all update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("DevOps App Running");
+  res.end("DevOps App Running and it is the upgraded version as it is running on jenkins using the docker");
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000 and it is the upgraded version as it is running on jenkins using the docker");
+  console.log("Server running on port 3000");
 });
