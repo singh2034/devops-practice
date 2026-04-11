4a129671aa8db16874161961cc91a635d13eea13
singh2034 <singhaayush2034@gmail.com>
2026-04-11 22:11:02 +0530

    updated v2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.end("DevOps App Running and it is the upgraded version as it is running on jenkins using the docker");
+  res.end("Let's check it again with the upgraded version which is version 2");
 });
 
 server.listen(3000, () => {
