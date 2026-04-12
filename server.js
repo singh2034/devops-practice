68bc537c56e9366d4b666ad79b1aa6a10d8c99a9
singh2034 <singhaayush2034@gmail.com>
2026-04-12 15:17:05 +0530

    updated - add metrics

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require("http");
+const client = require("prom-client");
+
+// collect default metrics (CPU, memory, etc.)
+client.collectDefaultMetrics();
+
+const requestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of requests",
+});
 
 const server = http.createServer((req, res) => {
-  res.end("DevOps App Running and it is the version 5 & automatic ci/cd pipeline test 2");
+  if (req.url === "/metrics") {
+    res.setHeader("Content-Type", client.register.contentType);
+    return res.end(client.register.metrics());
+  }
+
+  requestCounter.inc();
+
+  res.end("DevOps App Running with Metrics 🚀");
 });
 
 server.listen(3000, () => {
