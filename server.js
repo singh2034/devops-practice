10ecbfa5970906adbaa443f64991b21eaa37e50d
singh2034 <singhaayush2034@gmail.com>
2026-04-12 14:41:24 +0530

    update v4 test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("DevOps App Running and it is the version 4 & automatic ci/cd pipeline");
+  res.end("DevOps App Running and it is the version 5 & automatic ci/cd pipeline test 2");
 });
 
 server.listen(3000, () => {
